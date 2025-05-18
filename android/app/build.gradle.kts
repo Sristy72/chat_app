33ec67f2a9c919013c9c52473d7aac51ce7ffddb
isita <eshitamondol72@gmail.com>
2025-05-18 21:45:02 +0600

    s

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -27,7 +28,7 @@ android {
         applicationId = "com.example.chat_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- lib/screen/authentication_screen.dart
+++ lib/screen/authentication_screen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+final _firebase = FirebaseAuth.instance;
 class AuthenticationScreen extends StatefulWidget {
   const AuthenticationScreen({super.key});
 
@@ -19,82 +21,84 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
       backgroundColor: Color.fromARGB(255, 90, 88, 79),
       appBar: AppBar(title: Text('Authenticate Yourself')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 30, bottom: 20, left: 20, right: 20),
-              width: 200,
-              child: Image.asset('assets/images/chat.png'),
-            ),
-
-            Card(
-              margin: EdgeInsets.all(20),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormField(
-                          controller: _emailTEController,
-                          decoration: InputDecoration(
-                            labelText: 'Email Address',
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 30, bottom: 20, left: 20, right: 20),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+          
+              Card(
+                margin: EdgeInsets.all(20),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            controller: _emailTEController,
+                            decoration: InputDecoration(
+                              labelText: 'Email Address',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value){
+                              if(value == null || value.trim().isEmpty || !value.contains('@')){
+                                return 'Please enter a valid email address.';
+                              }
+                              return null;
+                            },
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                          validator: (value){
-                            if(value == null || value.trim().isEmpty || !value.contains('@')){
-                              return 'Please enter a valid email address.';
-                            }
-                            return null;
-                          },
-                        ),
 
-                        TextFormField(
-                          controller: _passwordTEController,
-                          decoration: InputDecoration(labelText: 'Password'),
-                          obscureText: true,
-                          validator: (value){
-                            if(value == null || value.trim().length < 6){
-                              return 'Password must be at least 6 characters long.';
-                            }
-                            return null;
-                          },
-                        ),
-
-                        SizedBox(height: 12),
-
-                        ElevatedButton(
-                          onPressed: () {_submit();},
-                          child: Text(_isLogin ? 'Login' : 'Sign Up'),
-                        ),
-
-                        SizedBox(height: 6),
-
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              
-                            });
-                            _isLogin = !_isLogin;
-                          },
-                          child: Text(
-                            _isLogin
-                                ? 'Create an account'
-                                : 'I already have an account',
+                          TextFormField(
+                            controller: _passwordTEController,
+                            decoration: InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value){
+                              if(value == null || value.trim().length < 6){
+                                return 'Password must be at least 6 characters long.';
+                              }
+                              return null;
+                            },
                           ),
-                        ),
-                      ],
+
+                          SizedBox(height: 12),
+
+                          ElevatedButton(
+                            onPressed: () {_submit();},
+                            child: Text(_isLogin ? 'Login' : 'Sign Up'),
+                          ),
+
+                          SizedBox(height: 6),
+
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+
+                              });
+                              _isLogin = !_isLogin;
+                            },
+                            child: Text(
+                              _isLogin
+                                  ? 'Create an account'
+                                  : 'I already have an account',
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -105,6 +109,11 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
       final enteredEmail = _emailTEController.text.trim();
       final enteredPassword = _passwordTEController.text.trim();
 
+      if(_isLogin){
+
+      }else{
+        _firebase.createUserWithEmailAndPassword(email: enteredEmail, password: enteredPassword);
+      }
     }
   }
 }
